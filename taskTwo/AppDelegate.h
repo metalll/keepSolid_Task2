5dbc2acb87112a91ea11bfbfe80cca85e6476ca1
Lexa <metall112@bk.ru>
2016-07-08 22:16:07 +0300

    Initial Commit

---
+++ taskTwo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  taskTwo
+//
+//  Created by NullStackDev on 08.07.16.
+//  Copyright © 2016 nullStackDev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ taskTwo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  taskTwo
+//
+//  Created by NullStackDev on 08.07.16.
+//  Copyright © 2016 nullStackDev. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
